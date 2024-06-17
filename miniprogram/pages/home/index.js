0864640be13130a18fd145b2096f44ab065fc018
peng <657805067@qq.com>
2024-06-17 14:38:50 +0800

    add swiper

--- miniprogram/pages/home/index.js
+++ miniprogram/pages/home/index.js
@@ -14,7 +14,10 @@ Page({
     defaultData: {
       title: "我的主页", // 导航栏标题
     },
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    background: [
+      'https://636c-cloud1-0gq8f3qi3903d318-1327253936.tcb.qcloud.la/app-assets/banner%20(1).png?sign=1fb30d417f9c1c1ac840157f136f42c6&t=1718604950',
+      'https://636c-cloud1-0gq8f3qi3903d318-1327253936.tcb.qcloud.la/app-assets/bg.png?sign=c85d97c2636daecf2ed1ad1fac89ec8a&t=1718605109',
+    ],
     indicatorDots: true,
     vertical: false,
     autoplay: false,
