835f98f8da4838b5a4ca38a9e15e43945a6d170a
小14 <rudanxu@whale.im>
2024-10-25 11:06:46 +0800

    feat 路书卡片

---
+++ miniprogram/components/path-note-list/card/index.js
@@ -0,0 +1,32 @@
+// components/path-note-list/card/index.js
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    title: "秋日北京 | 秋日花海当一天精灵",
+    joinedCount: 82,
+    pointCount: 5,
+    recommendCount: 3,
+    checkPointCount: 4,
+    rightCount: 6,
+    pointImage: "https://oss-whale-peach.meetwhale.com/wos%2Fharbor%2FKng-Ukv9oFXRMvunR2IR1%2FRectangle%20346240831.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
