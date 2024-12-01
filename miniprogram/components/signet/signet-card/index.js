1538b22874992c41c7c8367f317db0f4c1680c3c
小13 <rudanxu@whale.im>
2024-12-01 22:14:54 +0800

    feat 印章

---
+++ miniprogram/components/signet/signet-card/index.js
@@ -0,0 +1,28 @@
+// components/signet/signet-book/index.js
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    icon: String,
+    isBook: Boolean,
+    isFilled: Boolean,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
