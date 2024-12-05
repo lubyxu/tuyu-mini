dcb7d6cf1094e92a5b9ad65ed26ccc5c553ec839
小14 <rudanxu@whale.im>
2024-12-05 21:24:09 +0800

    feat tuyu

--- miniprogram/pages/signet-add-page/index.js
+++ miniprogram/pages/signet-add-page/index.js
@@ -1,4 +1,5 @@
-// pages/signet-add-page/index.js
+import dayjs from 'dayjs';
+const chooseLocation = requirePlugin('chooseLocation');
 Page({
   /**
    * 页面的初始数据
@@ -8,6 +9,7 @@ Page({
     src: '',
     name: '',
     time: '',
+    timeStr: '',
     location: '',
 
     isEdit: false,
@@ -18,7 +20,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const date = dayjs();
+    this.setData({
+      time: date.unix(),
+      timeStr: date.format('YYYY年YY月DD日 HH:mm:ss')
+    })
   },
 
   /**
@@ -32,7 +38,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    const location = chooseLocation.getLocation(); 
+    console.log('location:', location)
   },
 
   /**
@@ -62,6 +69,16 @@ Page({
   onReachBottom() {
 
   },
+
+  onChooseLocation () {
+		wx.chooseLocation({
+			success: (res) => {
+				this.setData({
+					location: res
+				});
+			}
+		});
+	},
 
   /**
    * 用户点击右上角分享
@@ -93,5 +110,19 @@ Page({
     this.setData({
       modalValue: e.detail.value
     });
+  },
+  onMapOpen() {
+    const key = 'LXABZ-P2ICT-ATAXX-VM4X3-LGHHE-ZBFHI';
+    const referer = 'fuyu';
+    const location = JSON.stringify({
+      latitude: 39.89631551,
+      longitude: 116.323459711
+    });
+    const category = '生活服务,娱乐休闲';
+     
+    wx.navigateTo({
+      url: 'plugin://chooseLocation/index?key=' + key + '&referer=' + referer + '&location=' + location + '&category=' + category
+    });
+    
   }
 })
