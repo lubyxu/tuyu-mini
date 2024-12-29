653fdb0d1f075f4872c565bb3d385d0a1166562d
小13 <rudanxu@whale.im>
2024-12-29 23:13:59 +0800

    fix 印章

--- miniprogram/components/signet/signet-container/index.js
+++ miniprogram/components/signet/signet-container/index.js
@@ -80,12 +80,13 @@ Component({
           }
           this.triggerEvent('onPageGo', { path: '/' + path });
         },
-        error() {
+        fail() {
+          console.log('error failed')
           wx.showToast({
             icon: 'none',
             title: '扫码失败',
           });
-        }
+        },
       })
     },
     async onStampChange() {
