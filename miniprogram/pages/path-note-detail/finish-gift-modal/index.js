348b3372ab8d47a1272f44e62848aea8e2670805
小14 <rudanxu@whale.im>
2025-06-12 09:52:39 +0800

    feat tuyu

---
+++ miniprogram/pages/path-note-detail/finish-gift-modal/index.js
@@ -0,0 +1,125 @@
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 控制弹窗显示隐藏
+    show: {
+      type: Boolean,
+      value: false
+    },
+    // 奖品列表数据
+    gifts: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // 默认奖品列表
+    giftList: [
+      {
+        icon: 'card_giftcard',
+        iconClass: 'red-gradient',
+        name: '惊喜好礼券'
+      },
+      {
+        icon: 'emoji_events',
+        iconClass: 'yellow-gradient',
+        name: '成就徽章一枚'
+      },
+      {
+        icon: 'star',
+        iconClass: 'green-gradient',
+        name: '幸运星积分'
+      },
+      {
+        icon: 'redeem',
+        iconClass: 'blue-gradient',
+        name: '神秘小包裹'
+      },
+      {
+        icon: 'workspace_premium',
+        iconClass: 'purple-gradient',
+        name: '尊享体验卡'
+      }
+    ]
+  },
+
+  /**
+   * 组件生命周期
+   */
+  observers: {
+    'gifts': function(newGifts) {
+      // 如果传入了奖品数据，使用传入的数据
+      if (newGifts && newGifts.length > 0) {
+        this.setData({
+          giftList: newGifts
+        });
+      }
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    /**
+     * 点击遮罩层关闭弹窗
+     */
+    onOverlayTap() {
+      this.hideModal();
+    },
+
+    /**
+     * 点击弹窗内容区域，阻止事件冒泡
+     */
+    onContentTap() {
+      // 阻止事件冒泡，不关闭弹窗
+    },
+
+    /**
+     * 点击确认按钮
+     */
+    onConfirm() {
+      // 触发确认事件，通知父组件
+      this.triggerEvent('confirm', {
+        gifts: this.data.giftList
+      });
+      // 关闭弹窗
+      this.hideModal();
+    },
+
+    /**
+     * 隐藏弹窗
+     */
+    hideModal() {
+      // 触发关闭事件，通知父组件
+      this.triggerEvent('close');
+    },
+
+    /**
+     * 显示弹窗
+     */
+    showModal() {
+      this.triggerEvent('show');
+    },
+
+    /**
+     * 设置奖品列表
+     * @param {Array} gifts 奖品数组
+     */
+    setGifts(gifts) {
+      this.setData({
+        giftList: gifts || this.data.giftList
+      });
+    }
+  }
+}) 
